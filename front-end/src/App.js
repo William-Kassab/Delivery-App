2c1327ec058fdda7a13d85fccaa8f5957dd62c40
enzo <enzo@pop-os.localdomain>
2022-06-01 16:37:14 -0300

    refactor: insert routes in app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import Routes from './Routes';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import LoginPage from './Pages/loginPage/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      {/* <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object> */}
-      <Routes />
-    </div>
+    <Routes>
+      <Route exact path="/" element={ <Navigate to="/login" /> } />
+      <Route exact path="/login" element={ <LoginPage /> } />
+      {/* <Route path="/register" element={ "<Register />" } /> */}
+    </Routes>
   );
 }
 
