8ce083736fb6f4d079d4eead8c2e896c2be5b369
Arthur Simões <arthursimoes155@gmail.com>
2022-06-01 18:06:25 -0300

    feat: valid login

--- front-end/src/Pages/loginPage/LoginPage.js
+++ front-end/src/Pages/loginPage/LoginPage.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function LoginPage() {
+  const [login, setLogin] = useState({
+    email: '',
+    password: '',
+  });
+
+  function handleChange({ target }) {
+    const { name, value } = target;
+    setLogin({
+      ...login,
+      [name]: value });
+  }
+
+  const regex = (/\S+@\S+\.\S+/);
+  const length = 5;
+  const passwordIsValid = login.password.length > length;
+  const isEmailValid = regex.test(login.email);
+
   return (
     <div>
       <div>
@@ -14,21 +31,28 @@ function LoginPage() {
             Login
             <input
               type="email"
+              name="email"
+              value={ login.email }
               data-testid="common_login__input-email"
               placeholder="email@trybeer.com.br"
+              onChange={ handleChange }
             />
           </label>
           <label htmlFor="password">
             Senha
             <input
               type="password"
+              name="password"
+              value={ login.password }
               data-testid="common_login__input-password"
               placeholder="***********"
+              onChange={ handleChange }
             />
           </label>
           <button
             type="button"
             data-testid="common_login__button-login"
+            disabled={ !(isEmailValid && passwordIsValid) }
           >
             Login
           </button>
