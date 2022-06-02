c7221c3178b357843f528586e5b378bba28b902c
enzo <enzo@pop-os.localdomain>
2022-06-02 17:44:41 -0300

    feat: CustomerProducts Page

--- front-end/src/Pages/customerProducts/CustomerProducts.jsx
+++ front-end/src/Pages/customerProducts/CustomerProducts.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import Navbar from '../../components/navbar';
+import React, { useEffect, useState } from 'react';
 
-const CustomerProducts = () => (
-  <Navbar />
-);
+import Navbar from '../../components/navbar';
+import Products from '../../components/products';
+import { getProducts } from '../../service/api';
+
+const CustomerProducts = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { token } = JSON.parse(localStorage.getItem('user'));
+      const { data } = await getProducts(token);
+      setProducts(data);
+    })();
+  }, []);
+  return (
+    <>
+      <Navbar />
+      <Products products={ products } />
+    </>
+  );
+};
 
 export default CustomerProducts;
