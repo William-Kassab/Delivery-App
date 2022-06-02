21c9263c1da7fa03bf9d01cec0c361bf9e979cb8
enzo <enzo@pop-os.localdomain>
2022-06-02 15:37:47 -0300

    feat: customer products with navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './Pages/loginPage/LoginPage';
 import RegisterPage from './Pages/registerPage/RegisterPage';
+import CustomerProducts from './Pages/customerProducts/CustomerProducts';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Route exact path="/" element={ <Navigate to="/login" /> } />
       <Route exact path="/login" element={ <LoginPage /> } />
       <Route path="/register" element={ <RegisterPage /> } />
-      <Route path="/customer/products" element={ <p> Customer </p> } />
+      <Route path="/customer/products" element={ <CustomerProducts /> } />
     </Routes>
   );
 }
