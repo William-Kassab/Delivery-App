ff941065489cc69dfc278839660b057be5168701
Renato Moreno <renats.mc@gmail.com>
2022-06-05 06:38:41 -0300

    feat: form validation

--- front-end/src/components/registerForm/RegisterForm.jsx
+++ front-end/src/components/registerForm/RegisterForm.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './registerForm.css';
 
 export default function RegisterForm() {
+  const [register, setRegister] = useState({
+    name: '',
+    email: '',
+    password: '',
+    role: 'customer',
+  });
+
+  function handleChange({ target }) {
+    const { name, value } = target;
+    setRegister({
+      ...register,
+      [name]: value });
+  }
+
+  const regex = /\S+@\S+\.\S+/;
+  const lengthEmail = 5;
+  const lengthName = 12;
+  const passwordIsValid = register.password.length > lengthEmail;
+  const isEmailValid = regex.test(register.email);
+  const nameIsValid = register.name.length >= lengthName;
+  const isValid = passwordIsValid && isEmailValid && nameIsValid;
+
   return (
     <form>
       <fieldset className="form-container">
@@ -10,8 +32,10 @@ export default function RegisterForm() {
           <input
             type="text"
             name="name"
+            value={ register.name }
             data-testid="admin_manage__input-name"
             placeholder="Nome e sobrenome"
+            onChange={ handleChange }
           />
         </label>
         <label htmlFor="email">
@@ -19,8 +43,10 @@ export default function RegisterForm() {
           <input
             type="email"
             name="email"
+            value={ register.email }
             data-testid="admin_manage__input-email"
             placeholder="email@trybeer.com.br"
+            onChange={ handleChange }
           />
         </label>
         <label htmlFor="password">
@@ -28,19 +54,31 @@ export default function RegisterForm() {
           <input
             type="password"
             name="password"
+            value={ register.password }
             data-testid="admin_manage__input-password"
             placeholder="***********"
+            onChange={ handleChange }
           />
         </label>
         <label htmlFor="role">
           Tipo
-          <select name="role" id="role" data-testid="admin_manage__select-role">
-            <option value="Vendedor">Vendedor</option>
-            <option value="Cliente">Cliente</option>
-            <option value="Administrador">Administrador</option>
+          <select
+            name="role"
+            id="role"
+            data-testid="admin_manage__select-role"
+            value={ register.role }
+            onChange={ handleChange }
+          >
+            <option value="customer">Cliente</option>
+            <option value="seller">Vendedor</option>
+            <option value="administrator">Administrador</option>
           </select>
         </label>
-        <button type="button" data-testid="admin_manage__button-register">
+        <button
+          type="button"
+          data-testid="admin_manage__button-register"
+          disabled={ !isValid }
+        >
           Cadastrar
         </button>
       </fieldset>
